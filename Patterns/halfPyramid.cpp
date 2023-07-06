988bdacf0a0eafa9a6ccc91a44de330f873c3f0b
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-06 20:52:46 +0530

    Half Pyramid Pattern added

---
+++ Patterns/halfPyramid.cpp
@@ -0,0 +1,20 @@
+// * 
+// * * 
+// * * * 
+// * * * *
+
+#include<iostream>
+using namespace std;
+#define ll long long
+int main()
+{
+	int row;
+	cin>>row;
+	for(int i=0; i<row; i++){
+		for(int j=0; j<i+1; j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+  return 0;
+}
