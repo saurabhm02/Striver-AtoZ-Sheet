8daa11c7531da7cd1266ad42889c57ddbb35fc58
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-07 09:33:53 +0530

    Inverted Full Pyramid pattern added

---
+++ Patterns/invertedFullPyramid.cpp
@@ -0,0 +1,26 @@
+// * * * * * 
+//  * * * * 
+//   * * * 
+//    * * 
+//     *
+
+#include<iostream>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int size;
+    cin>>size;
+
+    for(int i=0; i<size; i++){
+    	for(int j=0; j<i; j++){
+    		cout<<" ";
+    	}
+    	for(int j=0; j<size-i;j++){
+    		cout<<"* ";
+    	}
+    	cout<<endl;
+    }	
+    return 0;
+}
