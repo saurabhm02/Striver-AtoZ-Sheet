a5af7eaa9014e4e8ec8cea2cecc3abd46b3b797d
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-07 09:45:18 +0530

    Solid Diamand Pattern added

---
+++ Patterns/solidDiamand.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#define ll long long
+
+int main()
+{
+	int size;
+	cin>>size;
+    
+
+  // Full Pyramid
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size-i-1; j++){
+			cout<<" ";
+		}
+
+		for(int j=0; j<i+1; j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+  // inverted Pyramid
+	for(int i=0; i<size; i++){
+    	for(int j=0; j<i; j++){
+    		cout<<" ";
+    	}
+    	for(int j=0; j<size-i;j++){
+    		cout<<"* ";
+    	}
+    	cout<<endl;
+    }
+    return 0;
+}
