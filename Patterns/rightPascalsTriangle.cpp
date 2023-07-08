48162081da7a701b4f42201b86e60022881f3149
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-08 10:49:43 +0530

    right Pascals Triangle pattern added

---
+++ Patterns/rightPascalsTriangle.cpp
@@ -0,0 +1,27 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * 
+// * * 
+// *
+
+#include<iostream>
+using namespace std;
+#define ll long long
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i=1; i<=2*n - 1; i++){
+		int stars = i;
+		if(i>n) stars = 2*n - i;
+		for(int j=0; j<stars; j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+    return 0;
+}
