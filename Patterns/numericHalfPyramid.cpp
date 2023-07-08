2624299a841ac7f5c6704d7602f9cef69b9458fa
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-08 10:40:02 +0530

    Numeric Half Pyramid modified

--- Patterns/numericHalfPyramid.cpp
+++ Patterns/numericHalfPyramid.cpp
@@ -19,3 +19,12 @@ int main()
     }
     return 0;
 }
+
+//----------------2nd Way-------------------
+
+// for(int i=1; i<=size; i++){
+//     	for(int j=1; j<i; j++){
+//     		cout<<j;
+//     	}
+//     	cout<<endl;
+//     }
