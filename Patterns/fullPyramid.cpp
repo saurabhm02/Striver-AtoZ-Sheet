83141b299fb98f90725f7a905bd38233edc8e273
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-08 10:36:33 +0530

    Full Pyramid modified

--- Patterns/fullPyramid.cpp
+++ Patterns/fullPyramid.cpp
@@ -44,14 +44,19 @@ int main()
 // 	int size;
 // 	cin>>size;
 
-// 	for(int i=0; i<size; i++){
+// for(int i=0; i<size; i++){
+//   // Space
 // 		for(int j=0; j<size-i-1; j++){
-// 			cout<<" ";
-// 		}
-
-// 		for(int j=0; j<i+1; j++){
+// 			cout<<"  ";
+// 	 	}
+//   // Star
+// 		for(int j=0; j<2*i+1; j++){
 // 			cout<<"* ";
 // 		}
+//   // Space
+// 		for(int j=0; j<size-i-1; j++){
+// 			cout<<"  ";
+// 	 	}
 // 		cout<<endl;
 // 	}
 //     return 0;
