c213660df69bd13c94cbd9d8997b5dd4f7a91cce
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-08 10:38:17 +0530

    Hollow Inverted Half Pyramid modified

--- Patterns/hollowInvertedHalfPyramid.cpp
+++ Patterns/hollowInvertedHalfPyramid.cpp
@@ -25,3 +25,28 @@ int main()
     }	
     return 0;
 }
+
+
+
+
+#include<iostream>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+      //spaces
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+      //numers
+        for(int j=0; j=i+1; j++){
+            cout<<i+j+1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
