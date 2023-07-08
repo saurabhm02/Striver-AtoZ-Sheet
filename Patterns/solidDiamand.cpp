b89e80034fe002547f7e6675d87e751df6199301
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-08 10:41:06 +0530

    Solid Diamand Pattern modified

--- Patterns/solidDiamand.cpp
+++ Patterns/solidDiamand.cpp
@@ -1,3 +1,12 @@
+//       *       
+//     * * *     
+//   * * * * *   
+// * * * * * * * 
+// * * * * * * * 
+//   * * * * *   
+//     * * *     
+//       *
+
 #include<iostream>
 using namespace std;
 #define ll long long
@@ -6,28 +15,69 @@ int main()
 {
 	int size;
 	cin>>size;
-    
 
-  // Full Pyramid
-	for(int i=0; i<size; i++){
+// Upper Pyramid	
+    for(int i=0; i<size; i++){
+  // Space
 		for(int j=0; j<size-i-1; j++){
-			cout<<" ";
-		}
-
-		for(int j=0; j<i+1; j++){
+			cout<<"  ";
+	 	}
+  // Star
+		for(int j=0; j<2*i+1; j++){
 			cout<<"* ";
 		}
+  // Space
+		for(int j=0; j<size-i-1; j++){
+			cout<<"  ";
+	 	}
 		cout<<endl;
 	}
-  // inverted Pyramid
+
+// Lower Pyramid
 	for(int i=0; i<size; i++){
-    	for(int j=0; j<i; j++){
-    		cout<<" ";
-    	}
-    	for(int j=0; j<size-i;j++){
-    		cout<<"* ";
-    	}
-    	cout<<endl;
+      //space  
+        for(int j=0; j<i; j++){
+            cout<<"  ";
+        }
+
+      //Stars  
+        for(int j=0; j<2*size - (2*i + 1);j++){
+            cout<<"* ";
+        }
+
+      //Space
+        for(int j=0; j<i;j++){
+            cout<<"  ";
+        } 
+        cout<<endl;
     }
+
+
+  
     return 0;
 }
+
+
+// ---------------2nd Way------------------
+
+// // Full Pyramid
+// 	for(int i=0; i<size; i++){
+// 		for(int j=0; j<size-i-1; j++){
+// 			cout<<" ";
+// 		}
+
+// 		for(int j=0; j<i+1; j++){
+// 			cout<<"* ";
+// 		}
+// 		cout<<endl;
+// 	}
+//   // inverted Pyramid
+// 	for(int i=0; i<size; i++){
+//     	for(int j=0; j<i; j++){
+//     		cout<<" ";
+//     	}
+//     	for(int j=0; j<size-i;j++){
+//     		cout<<"* ";
+//     	}
+//     	cout<<endl;
+//     }
