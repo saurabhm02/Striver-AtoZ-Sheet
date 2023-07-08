7c08342de44e92aa99563b755ee61dbbcb29f6b8
Saurabh <saurabhmahapatra677@gmail.com>
2023-07-08 10:39:14 +0530

    Inverted Full Pyramid modified

--- Patterns/invertedFullPyramid.cpp
+++ Patterns/invertedFullPyramid.cpp
@@ -1,8 +1,7 @@
-// * * * * * 
-//  * * * * 
-//   * * * 
-//    * * 
-//     *
+// * * * * * * * 
+//   * * * * *   
+//     * * *     
+//       *
 
 #include<iostream>
 using namespace std;
@@ -12,15 +11,37 @@ int main()
 {
     int size;
     cin>>size;
-
+    
     for(int i=0; i<size; i++){
-    	for(int j=0; j<i; j++){
-    		cout<<" ";
-    	}
-    	for(int j=0; j<size-i;j++){
-    		cout<<"* ";
-    	}
-    	cout<<endl;
-    }	
+      //space  
+        for(int j=0; j<i; j++){
+            cout<<"  ";
+        }
+
+      //Stars  
+        for(int j=0; j<2*size - (2*i + 1);j++){
+            cout<<"* ";
+        }
+
+      //Space
+        for(int j=0; j<i;j++){
+            cout<<"  ";
+        } 
+        cout<<endl;
+    }
+    	
     return 0;
 }
+
+
+//--------------------2nd Way-------------
+
+// for(int i=0; i<size; i++){
+//         for(int j=0; j<i; j++){
+//             cout<<" ";
+//         }
+//         for(int j=0; j<size-i;j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+// }
