6d39e1304df8f72ca6de13eda298fcfb5f10c40a
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-10-11 13:19:26 +0530

    solution of Last Occurrence in a Sorted Array

---
+++ 02. Binary Search/01. Easy/07 Last occurrence in a sorted array.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+#define ll long long
+
+// Brute force Approach
+// Approach -> Linear Search
+
+// 1) As the array is already sorted, start traversing the array from the last using a for loop and check whether the element is present or not.
+// 2) If the target element is present, break out of the loop and print the resulting index.
+// 3) If the target element is not present inside the array, then print -1
+
+int lasttOccurence(vector<int> &arr, int n, int target){
+	for(int i=n-1; i>=0; i--){
+		if(arr[i] == target){
+			return i;
+			break;
+		}
+	}
+	return -1;
+	// T.c : O(n)
+	// S.c : O(1)
+}
+
+// Optimal Approach 
+// Approach -> Binary Search
+
+// If the mid element is equal to the key value, store the mid-value in the result and move the start pointer to mid + 1(move leftward)
+// Else if the key value is less than the mid element then end= mid-1(move leftward)
+// Else do start = mid+1 (move rightwards)
+
+int lasttOccurence2(vector<int> &arr, int n, int target){
+	int s = 0;
+	int e = arr.size() - 1;
+	int ans = -1;
+
+	while(s <= e){
+		int mid = s + (e - s) / 2;
+		if(arr[mid] == target){
+			ans = mid;
+			s = mid + 1;
+		}
+		else if(arr[mid] < target){
+			s = mid + 1;
+		}
+		else{
+			e = mid - 1;
+		}
+	}
+	return ans;
+
+	// T.c : O(logn)
+	// S.c : O(1)
+}
+int main()
+{
+	int n;
+	cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int target;
+    cin>>target;
+	cout<<lasttOccurence2(arr, n, target);
+    return 0;
+}
